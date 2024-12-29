c28e6d6f246203b023e3bacbd0eaf186b30cf908
justsanchez <alvarosanchez9261@gmail.com>
2024-12-29 01:31:41 -0500

    saving validation

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -6,6 +6,9 @@ export default function UserForm(){
     // ! i set it as a array and its nottttt
     // const [setName] = useContext(UserContext);
     const {name, setName} = useContext(UserContext);
+    
+
+    const [error, setError] = useState(false);
 
     function handleChange(event){
         setInputName(event.target.value);
@@ -15,27 +18,34 @@ export default function UserForm(){
     function handleSubmit(event){
         event.preventDefault();
 
+        if (inputName == null | inputName == ''){
+            setError(true);
+            return;
+        }
+
         // setting the name in context
         setName(inputName); 
         // changing the URL without reloading the page
         window.history.pushState({}, '', '/quiz');
         const navEvent = new PopStateEvent('popstate');
         // dispatch a navigation event
+
         window.dispatchEvent(navEvent);
     }
-    // ! attempt to do conditional rendering with the name
 
     return (
         // add the form here
         <>
+
         {name && <h2>Reload the page to restart.</h2>}
-        {!name && 
-            <form onSubmit={handleSubmit}>
+
+        <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Name: </label>
                 <input type="text" id="name" onChange={handleChange}/>
                 <button type="submit">Submit</button>
             </form>
-        }
+
+        {error && <p className="errorStyle">Please enter a name to continue...</p>}
         </>
     )
 
